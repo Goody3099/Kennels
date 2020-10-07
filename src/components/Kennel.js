c587b33c7a057c97601fa5c317dac0bd0cd1ccf8
Goody <mattgoodman3099@gmail.com>
2020-10-07 11:56:17 -0500

    log in and add user

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,61 +1,33 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
-import "./Kennel.css"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Kennel.css";
 
 export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
-)
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
 
-
-// import React from "react"
-// import { AnimalCard } from "./animal/AnimalCard"
-// import "./animal/Animal.css"
-// import { LocationCard } from "./Location/Location"
-// import { CustomerCard } from "./Customer/Customer"
-// import { EmployeeCard } from "./Employee/Employee"
-
-// export const Kennel = () => (
-//     <>
-//         <h2>Nashville Kennels</h2>
-//         <small>Loving care when you're not there.</small>
-
-//         <address>
-//             <div>Visit Us at the Nashville North Location</div>
-//             <div>500 Puppy Way</div>
-//         </address>
-
-//         <h2>Animals</h2>
-//         <article className="animals">
-//             <AnimalCard />
-//             <AnimalCard />
-//             <AnimalCard />
-//         </article>
-
-//         <h2>Locations</h2>
-//         <article className="locations">
-//             <LocationCard />
-//             <LocationCard />
-//         </article>
-
-//         <h2>Customers</h2>
-//         <article className="customers">
-//             <CustomerCard />
-//             <CustomerCard />
-//             <CustomerCard />
-//             <CustomerCard />
-//         </article>
-
-//         <h2>Employees</h2>
-//         <article className="employees">
-//             <EmployeeCard />
-//             <EmployeeCard />
-//             <EmployeeCard />
-//             <EmployeeCard />
-//         </article>
-//     </>
-// )
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
